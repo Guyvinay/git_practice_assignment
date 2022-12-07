693786271ff1e0042efec7256b47090c68d917f9
yogeshpm96 <yogeshmahajan500@gmail.com>
2022-12-07 20:11:49 +0530

    code_fix

--- prime_no.js
+++ prime_no.js
@@ -1,16 +1,24 @@
-  function prime_no(num){
+  function prime_no(num)
+{
 
     let count=0;
+
     for(let i=0; i<=num;i++){
-     if(num%i==1){
+     
+	if(num%i==0){
       count++
    }
-  }if(count==0){
+
+  }
+	if(count==2){
      console.log("Yes it is Prime")
-}
+	}
     else{
 
     console.log("Not a Prime")
+	}
+
 }
 
-  }
+
+prime_no(13);
